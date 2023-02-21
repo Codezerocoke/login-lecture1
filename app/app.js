c13c357e8714b73a99158af961a4c04753b83cc5
June <junhyeongbag51@gmail.com>
2023-02-21 17:57:18 +0900

    일단 바디파서 삭제

--- app/app.js
+++ app/app.js
@@ -6,7 +6,6 @@ const express = require("express");
 const app = express();
 
 
-
 //라우팅
 const home = require("./src/routes/home");
 
@@ -14,8 +13,9 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
+
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use("/", home); //use = 미들웨어 등록 메소드
 
--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,8 +1,8 @@
 "use strict";
 
-const id = document.querySelector("#id"),
-    psword = document.querySelector("#psword"),
-    loginBtn = document.querySelector("button");
+const id = document.querySelector("#id");
+const psword = document.querySelector("#psword");
+const loginBtn = document.querySelector("button");
 
 loginBtn.addEventListener("click", login);
 
