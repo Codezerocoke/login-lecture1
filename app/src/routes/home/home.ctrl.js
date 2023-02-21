cc21988b4baa0353a8be3df9638f194c52c75487
June <junhyeongbag51@gmail.com>
2023-02-21 21:51:12 +0900

    로그인기능 좋네

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -10,9 +10,29 @@ const output = {
     },
 };
 
+const users = {
+    id: ["park", "박", "준형"],
+    psword: ["123", "1234", "12345"],
+};
+
 const process = {
     login: (req, res) => {
-        console.log(req.body);
+        const id = req.body.id;
+        const psword = req.body.psword;
+
+        if(users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하였습니다.",
+        });
     },
 };
 
